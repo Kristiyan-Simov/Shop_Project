29d6b083384afa1dd981b4ea8982c32c9c7d6dde
Kristiyan_Simov <simov.kristian@gmail.com>
2024-06-10 14:40:51 +0300

    Added File Writing And Reading Functionality for Receipts

--- src/Main/Models/ReceiptHandler.java
+++ src/Main/Models/ReceiptHandler.java
@@ -4,8 +4,11 @@ import Main.Models.Contracts.ICashier;
 import Main.Models.Contracts.IProduct;
 import Main.Models.Contracts.IReceipt;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.io.*;
 
 public class ReceiptHandler {
     private static int createdReceipts;
@@ -20,10 +23,37 @@ public class ReceiptHandler {
     }
 
     public static String saveReceipt(Receipt receipt) {
-        return null;
+        String fileName = "Receipts/" + "receipt_" + receipt.getID() + ".txt";
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(receipt.toString());
+        } catch (IOException e){
+            System.err.println("Error saving the receipt to file: " + e.getMessage());
+        }
+
+        return receipt.toString();
+    }
+
+    public static String readReceiptFromFile(String serialNumber) {
+        String fileName = "Receipts/" + "receipt_" + serialNumber + ".txt";
+        StringBuilder content = new StringBuilder();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            while (line!= null){
+                content.append(line).append("\n");
+                line = reader.readLine();
+            }
+        }
+        catch (Exception e){
+            throw new UnsupportedOperationException(String.format("Error reading receipt from file: %s", e.getMessage()));
+        }
+
+        return content.toString();
     }
 
     //TODO
     // - Add Unit Tests
-    // - Add File Writing Functionality
 }
